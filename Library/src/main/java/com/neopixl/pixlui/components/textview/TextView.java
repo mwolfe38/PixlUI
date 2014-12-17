14f9071d9f3c88558cb65a0da3b8d5ae4876d285
Matt Wolfe <matt@evidence.com>
2014-12-17 03:03:21 -0800

    attempt at implementing <nowrap> functionality around text. Sort of works.

--- Library/src/main/java/com/neopixl/pixlui/components/textview/TextView.java
+++ Library/src/main/java/com/neopixl/pixlui/components/textview/TextView.java
@@ -23,6 +23,7 @@ import android.content.res.TypedArray;
 import android.graphics.Paint;
 import android.graphics.Typeface;
 import android.os.Build;
+import android.text.Editable;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.SpannableStringBuilder;
@@ -45,7 +46,7 @@ import static com.neopixl.pixlui.intern.PixlUIConstants.*;
  * 
  * @author Olivier Demolliens. @odemolliens Dev with Neopixl
  */
-public class TextView extends EllipsizingTextView {
+public class TextView extends android.widget.TextView {
 
 
     public static final float LETTER_SPACING_NORMAL = 1.0f;
@@ -54,13 +55,24 @@ public class TextView extends EllipsizingTextView {
 
     private float mLetterSpacing = LETTER_SPACING_NORMAL;
 
+    private static final String NEWLINE_STR = "\n";
+    private static final char NEWLINE_CHAR = '\n';
+
+    private static final String NOWRAP_START = "<nowrap>";
+    private static final String NOWRAP_END = "</nowrap>";
+    private static final int NOWRAP_LEN = NOWRAP_START.length();
+    private static final int NOWRAP_END_LEN = NOWRAP_END.length();
+
     private CharSequence mOriginalText;
 
     private CharSequence mLastSpacingAppliedToString = null;
 
-	public TextView(Context context) {
-		this(context,false);
-	}
+    private BufferType mBufferType = BufferType.NORMAL;
+
+
+    public TextView(Context context) {
+        super(context);
+    }
 
 	public TextView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -68,34 +80,122 @@ public class TextView extends EllipsizingTextView {
         setAllCaps(context, attrs, 0);
         setLetterSpacing(context, attrs, 0);
 	}
+//
+//    public TextView(Context context) {
+//        this(context,false);
+//    }
 
 	public TextView(Context context, AttributeSet attrs, int defStyle) {
-		this(context, attrs, defStyle,false);
+		super(context, attrs, defStyle);
 	}
+//
+//	public TextView(Context context, boolean canBeEllipsized) {
+//		this(context, null, canBeEllipsized);
+//	}
 
-	public TextView(Context context, boolean canBeEllipsized) {
-		this(context, null, canBeEllipsized);
-	}
+//	public TextView(Context context, AttributeSet attrs, boolean canBeEllipsized) {
+//		super(context, attrs, canBeEllipsized);
+//        setCustomFont(context, attrs, 0);
+//        setAllCaps(context, attrs, 0);
+//        setLetterSpacing(context, attrs, 0);
+//	}
+//
+//	public TextView(Context context, AttributeSet attrs, int defStyle, boolean canBeEllipsized) {
+//		super(context, attrs, defStyle,canBeEllipsized);
+//		setCustomFont(context, attrs, defStyle);
+//        setAllCaps(context, attrs, defStyle);
+//        setLetterSpacing(context, attrs, defStyle);
+//	}
 
-	public TextView(Context context, AttributeSet attrs, boolean canBeEllipsized) {
-		super(context, attrs, canBeEllipsized);
-        setCustomFont(context, attrs, 0);
-        setAllCaps(context, attrs, 0);
-        setLetterSpacing(context, attrs, 0);
-	}
 
-	public TextView(Context context, AttributeSet attrs, int defStyle, boolean canBeEllipsized) {
-		super(context, attrs, defStyle,canBeEllipsized);
-		setCustomFont(context, attrs, defStyle);
-        setAllCaps(context, attrs, defStyle);
-        setLetterSpacing(context, attrs, defStyle);
-	}
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        Editable editable = getEditableText();
+        if (editable == null) {
+            return;
+        }
+        int width = getWidth() - getPaddingLeft() - getPaddingRight();
+        if (width == 0) {
+            return;
+        }
 
+
+
+        Paint p = getPaint();
+        float[] widths = new float[editable.length()];
+        p.getTextWidths(editable.toString(), widths);
+        float curWidth = 0.0f;
+        int lastWSPos = -1;
+        int strPos = 0;
+        boolean reset = false;
+        int insertCount = 0;
+
+        String chars = editable.toString();
+        boolean containsNowrap = chars.contains(NOWRAP_START) && chars.contains(NOWRAP_END);
+
+
+        /*
+         * Traverse the string from the start position, adding each character's width to the total
+         * until: 1) A whitespace character is found. In this case, mark the whitespace position. If
+         * the width goes over the max, this is where the newline will be inserted. 2) A newline
+         * character is found. This resets the curWidth counter. curWidth > width. Replace the
+         * whitespace with a newline and reset the counter.
+         */
+        boolean inNowrap = false;
+        while (strPos < editable.length()) {
+            curWidth += widths[strPos];
+
+            char curChar = editable.charAt(strPos);
+
+            if (curChar == NEWLINE_CHAR) {
+                reset = true;
+            } else if (!inNowrap && Character.isWhitespace(curChar)) {
+                lastWSPos = strPos;
+            } else {
+                if (containsNowrap && curChar == '<') {
+                    char[] dest = new char[editable.length()-strPos];
+                    editable.getChars(strPos, editable.length(), dest, 0);
+                    String test = new String(dest);
+                    if (test.startsWith(NOWRAP_START) && test.contains(NOWRAP_END)) {
+                        inNowrap = true;
+                        editable.replace(strPos, strPos + NOWRAP_LEN, "");
+                    } else if (test.startsWith(NOWRAP_END)) {
+                        inNowrap = false;
+                        editable.replace(strPos, strPos + NOWRAP_END_LEN, "");
+                    }
+                }
+                if (curWidth > width && lastWSPos >= 0) {
+                    editable.replace(lastWSPos, lastWSPos + 1, NEWLINE_STR);
+                    insertCount++;
+                    strPos = lastWSPos;
+                    lastWSPos = -1;
+                    reset = true;
+                }
+            }
+
+            if (reset) {
+                curWidth = 0.0f;
+                reset = false;
+            }
+            strPos++;
+        }
+
+        if (insertCount != 0) {
+            setText(editable);
+        }
+    }
 
     @Override
     public void setText(CharSequence text, BufferType type) {
         if (isInEditMode()) {
             super.setText(text, type);
+            return;
+        }
+        if (mBufferType == BufferType.NORMAL && type != BufferType.NORMAL) {
+            mBufferType = BufferType.SPANNABLE;
+            if (mBufferType == BufferType.SPANNABLE && type == BufferType.EDITABLE) {
+                mBufferType = BufferType.EDITABLE;
+            }
         }
         if (text != null) {
             if (text instanceof Spannable) {
@@ -103,12 +203,12 @@ public class TextView extends EllipsizingTextView {
                 ScaleXSpan[] spans = spannable.getSpans(0, text.length(), ScaleXSpan.class);
                 if (spans == null || spans.length == 0) {
                     mOriginalText = text;
-                    applyLetterSpacing();
+                    applyLetterSpacing(mBufferType);
                     return;
                 }
             }
             mOriginalText = text;
-            applyLetterSpacing();
+            applyLetterSpacing(mBufferType);
             return;
         }
         super.setText(text, type);
@@ -125,17 +225,17 @@ public class TextView extends EllipsizingTextView {
 
     public void setLetterSpacing(float spacing) {
         mLetterSpacing = spacing;
-        applyLetterSpacing();
+        applyLetterSpacing(mBufferType);
     }
 
     public float getLettersSpacing() {
         return mLetterSpacing;
     }
 
-    private void applyLetterSpacing() {
+    private void applyLetterSpacing(BufferType type) {
 
         if (mOriginalText == null || mOriginalText.length() < 2 || mLetterSpacing == LETTER_SPACING_NORMAL || mLetterSpacing == 0.0f) {
-            super.setText(mOriginalText, BufferType.NORMAL);
+            super.setText(mOriginalText, type);
             return;
         }
 
@@ -148,7 +248,10 @@ public class TextView extends EllipsizingTextView {
                     Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         }
         mLastSpacingAppliedToString = mOriginalText;
-        super.setText(builder, BufferType.SPANNABLE);
+        if (mBufferType == BufferType.NORMAL) {
+            mBufferType = BufferType.SPANNABLE;
+        }
+        super.setText(builder, mBufferType);
     }
 
 	/**
@@ -187,7 +290,7 @@ public class TextView extends EllipsizingTextView {
 
 
     private void setLetterSpacing(Context ctx, AttributeSet attrs, int defStyle) {
-        TypedArray a = ctx.getTheme().obtainStyledAttributes(attrs, R.styleable.com_neopixl_pixlui_components_textview_TextView, 0,0);
+        TypedArray a = ctx.getTheme().obtainStyledAttributes(attrs, R.styleable.com_neopixl_pixlui_components_textview_TextView, R.styleable.com_neopixl_pixlui_components_textview_TextView_letterSpacing, defStyle);
         try {
             setLetterSpacing(a.getFloat(R.styleable.com_neopixl_pixlui_components_textview_TextView_letterSpacing, LETTER_SPACING_NORMAL));
         }finally {
